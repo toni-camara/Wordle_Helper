a391533a8c32c591af0d7837da2437395c9eda86
Toni <eyquepasa@gmail.com>
2022-01-23 21:22:13 +0100

    Filter Button working. activity_search.xml now gets the list of words from a file, and filters words containing a letter (hardstring for now)

--- app/src/main/java/android/example/wordlehelper/myMethods.kt
+++ app/src/main/java/android/example/wordlehelper/myMethods.kt
@@ -1,16 +1,24 @@
 package android.example.wordlehelper
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 import java.io.IOException
+import android.widget.RelativeLayout
+import androidx.core.view.marginTop
+
 
 class myMethods {
 
     /** This method takes a list of words from a file and stores them into a list */
-    fun readWordsFromFile(context: Context): List<String>{
+    fun readWordsFromFile(context: Context): List<String> {
         var string = ""
         try {
             val inputStream = context.assets.open("example.txt")
@@ -25,11 +33,27 @@ class myMethods {
         }
 
 
-
         val list = string.split("\n")
         return list.sorted()
 
     }
+
+    /** This method takes a Character input and searches the list for words containing that input */
+    fun searchForInput(wordList: List<String>, input: String): List<String> {
+        val wordsWithInput = mutableListOf<String>()
+        println("Your input was: $input . looking for words containing $input...\n")
+
+        for (word in wordList) {
+            for (letter in word.lowercase()) {
+                if (input == letter.toString()) {
+                    wordsWithInput.add(word)
+                }
+            }
+        }
+        wordsWithInput.forEach { println(it) }
+        return wordsWithInput
+    }
+
 
 }
 
--- app/src/main/java/android/example/wordlehelper/search.kt
+++ app/src/main/java/android/example/wordlehelper/search.kt
@@ -1,44 +1,91 @@
 package android.example.wordlehelper
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.widget.TextView
-import androidx.core.view.marginTop
 
 
 class search : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
+        var listIsFull: Boolean = false
+        var filteredListIsFull: Boolean = false
+        var filteredList: List<String> = listOf()
         val wordList = mutableListOf<String>()
 
-
+        /** Click on SHOW LIST Button*/
         val showListButton = findViewById<Button>(R.id.showListBtn)
         showListButton.setOnClickListener {
-            //var texto: String = myMethods().readWordsFromFile(this@search).joinToString { "\n" }
-            var texto:List<String> = myMethods().readWordsFromFile(this@search)
-            var lineCounter = 1
 
-            for(word in texto) {
+            var completeList: List<String> = myMethods().readWordsFromFile(this@search)
+            findViewById<LinearLayout>(R.id.verticalWordsLeft).removeAllViews()
+            findViewById<LinearLayout>(R.id.verticalWordsRight).removeAllViews()
+
+            var lineCounter = 1 // Used to count a maximum of 4 lines of words
+
+            for (word in completeList) {
+                if (!listIsFull) wordList.add(word) // Protection against duplicity
+
+                // Word list settings
                 val vistaNueva = TextView(this)
                 vistaNueva.textSize = 20f
-                vistaNueva.text = word
+                vistaNueva.text = word.uppercase()
+                vistaNueva.setTextColor(Color.parseColor("#6746c3"))
+                val typeface = Typeface.SANS_SERIF
+                vistaNueva.setTypeface(typeface)
 
-
-                if(lineCounter <= 4) {
+                //Word list printing
+                if (lineCounter <= 4) {
                     findViewById<LinearLayout>(R.id.verticalWordsLeft).addView(vistaNueva)
-                }
-                else if(lineCounter <=8) {
+                } else if (lineCounter <= 8) {
                     findViewById<LinearLayout>(R.id.verticalWordsRight).addView(vistaNueva)
                 }
-                lineCounter ++
+                lineCounter++
             }
 
+            listIsFull = true
+        }
+
+        /** Click on FILTER LETTER Button*/
+        val filterButton = findViewById<Button>(R.id.filterBtn)
+        filterButton.setOnClickListener {
+
+            if (wordList.isNotEmpty()) { // Verifies that the list has been obtained from the file
+                var input: String = "r" //Placeholder Search Criteria
+
+                if (!filteredListIsFull) filteredList = myMethods().searchForInput(wordList, input)
+
+                findViewById<LinearLayout>(R.id.verticalWordsLeft).removeAllViews()
+                findViewById<LinearLayout>(R.id.verticalWordsRight).removeAllViews()
+
+                var lineCounter = 1
+                for (word in filteredList) {
+
+                    // Word list settings
+                    val vistaNueva = TextView(this)
+                    vistaNueva.textSize = 20f
+                    vistaNueva.text = word.uppercase()
+                    vistaNueva.setTextColor(Color.parseColor("#6746c3"))
+                    val typeface =
+                        Typeface.SANS_SERIF//createFromAsset(applicationContext.assets, "sans-serif-light")
+                    vistaNueva.setTypeface(typeface)
+
+                    //Word list printing
+                    if (lineCounter <= 4) {
+                        findViewById<LinearLayout>(R.id.verticalWordsLeft).addView(vistaNueva)
+                    } else if (lineCounter <= 8) {
+                        findViewById<LinearLayout>(R.id.verticalWordsRight).addView(vistaNueva)
+                    }
+                    lineCounter++
+                }
+                filteredListIsFull = true
+
+            }
         }
     }
 }
