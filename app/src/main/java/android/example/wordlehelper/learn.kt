a72cf2268f4aa7ff43f7c7a9e394b09247f1e6c1
Toni <eyquepasa@gmail.com>
2022-01-29 21:48:48 +0100

    Revamped GUI. Added icons for buttons. Added "learn" section.

---
+++ app/src/main/java/android/example/wordlehelper/learn.kt
@@ -0,0 +1,11 @@
+package android.example.wordlehelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class learn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn)
+    }
+}
