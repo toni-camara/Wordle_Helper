5443c07c0a421fa015693b0dbf755fcbf3df85fa
Toni <eyquepasa@gmail.com>
2022-02-01 20:19:43 +0100

    Added bottom sheet. Changed layout distribution and theme

--- app/src/main/java/android/example/wordlehelper/MainActivity.kt
+++ app/src/main/java/android/example/wordlehelper/MainActivity.kt
@@ -2,41 +2,90 @@ package android.example.wordlehelper
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import java.io.File
 
 
+class ModalBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.modal_bottom_sheet_content, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val profileBtn = view.findViewById<Button>(R.id.profileBtn)
+        profileBtn.setOnClickListener {
+            val intent = Intent(activity, game::class.java)
+            startActivity(intent)
+        }
+
+        //CODE BUTTON
+        val codeButton = view.findViewById<Button>(R.id.codeBtn)
+        codeButton.setOnClickListener {
+            val intent = Intent(activity, code::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+
+
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+
+}
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //BOTTOM SHEET
+        val modalBottomSheet = ModalBottomSheet()
+        val bottomSheetButton = findViewById<Button>(R.id.bottomSheetExpandButton)
+        bottomSheetButton.setOnClickListener {
+            modalBottomSheet.show(supportFragmentManager, ModalBottomSheet.TAG)
+        }
+
+
+        //PLAY BUTTON
         val playButton = findViewById<Button>(R.id.playBtn)
         playButton.setOnClickListener {
             val intent = Intent(this, game::class.java)
             startActivity(intent)
         }
 
+        //SEARCH BUTTON
         val searchButton = findViewById<Button>(R.id.searchBtn)
         searchButton.setOnClickListener {
             val intent = Intent(this, search::class.java)
             startActivity(intent)
         }
 
+        //LEARN BUTTON
         val learnButton = findViewById<Button>(R.id.learnBtn)
         learnButton.setOnClickListener {
             val intent = Intent(this, learn::class.java)
             startActivity(intent)
         }
 
-        val codeButton = findViewById<Button>(R.id.codeBtn)
-        codeButton.setOnClickListener {
-            val intent = Intent(this, code::class.java)
-            startActivity(intent)
-        }
+
     }
 }
 
